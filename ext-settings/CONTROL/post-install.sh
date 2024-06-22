cd39f16741fe1b4883c25cf863160d6ab14c31fe
GD <pea@invo.fr>
2024-06-22 16:20:49 +0200

    possibility to use float

--- ext-settings/CONTROL/post-install.sh
+++ ext-settings/CONTROL/post-install.sh
@@ -6,8 +6,8 @@ if [ ! -f "/share/Ext Settings/settings.conf" ]; then cp ./default.conf "/share/
 
 . "/share/Ext Settings/settings.conf"
 
-sed -i "s/WARNING:[0-9]\+/WARNING:$EXT4_WARNING/g" /volume0/usr/builtin/webman/portal/build/launcherBuild.js
-sed -i "s/LIMIT:[0-9]\+/LIMIT:$EXT4_LIMIT/g" /volume0/usr/builtin/webman/portal/build/launcherBuild.js
+sed -i "s/WARNING:[0-9.]\+/WARNING:$EXT4_WARNING/g" /volume0/usr/builtin/webman/portal/build/launcherBuild.js
+sed -i "s/LIMIT:[0-9.]\+/LIMIT:$EXT4_LIMIT/g" /volume0/usr/builtin/webman/portal/build/launcherBuild.js
 sed -i "s/TOBE_EXHAUSTED_PERCENT:[0-9]\+/TOBE_EXHAUSTED_PERCENT:$BTRFS_FREE_LIMIT/g" /volume0/usr/builtin/webman/portal/build/launcherBuild.js
 
 exit 0
