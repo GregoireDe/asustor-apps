03272fe607d787da04adbfd72337342c37a4ef3a
GD <pea@invo.fr>
2024-12-29 15:20:34 +0100

    add docker privilege for igpu

--- immich/CONTROL/post-install.sh
+++ immich/CONTROL/post-install.sh
@@ -4,9 +4,30 @@ echo "post-install"
 
 . "/usr/local/AppCentral/immich-docker/CONTROL/conf.sh"
 
-wget -O docker-compose.yml $IMMICH_REPO/download/docker-compose.yml
-wget -O hwaccel.transcoding.yml $IMMICH_REPO/download/hwaccel.transcoding.yml
-wget -O hwaccel.ml.yml $IMMICH_REPO/download/hwaccel.ml.yml
+FILE_DOCKER_COMPOSE="docker-compose.yml"
+FILE_TRANSCODING="hwaccel.transcoding.yml"
+FILE_ML="hwaccel.ml.yml"
+
+if [ ! -f $FILE_DOCKER_COMPOSE ]; then
+  wget -O $FILE_DOCKER_COMPOSE $IMMICH_REPO/download/$FILE_DOCKER_COMPOSE.default
+  cp $FILE_DOCKER_COMPOSE.default $FILE_DOCKER_COMPOSE
+fi
+
+
+if [ ! -f $FILE_TRANSCODING ]; then
+  wget -O $FILE_TRANSCODING $IMMICH_REPO/download/$FILE_TRANSCODING.default
+  cp $FILE_TRANSCODING.default $FILE_TRANSCODING
+fi
+
+if [ ! -f $FILE_ML ]; then
+  wget -O $FILE_ML $IMMICH_REPO/download/$FILE_ML.default
+  cp $FILE_ML.default $FILE_ML
+fi
+
+mkdir -p  /share/Docker/$APP_NAME/config
+ln -s  $FILE_DOCKER_COMPOSE /share/Docker/$APP_NAME/$FILE_DOCKER_COMPOSE
+ln -s  $FILE_TRANSCODING /share/Docker/$APP_NAME/$FILE_TRANSCODING
+ln -s  $FILE_ML /share/Docker/$APP_NAME/$FILE_ML
 
 docker compose up -d
 
