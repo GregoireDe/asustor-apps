bc110ce13ac53a57c670e3b9586ec02a00466892
GD <pea@invo.fr>
2025-01-10 13:25:18 +0100

    attach arr apps to the same network

--- homarr/CONTROL/conf.sh
+++ homarr/CONTROL/conf.sh
@@ -3,3 +3,4 @@
 APP_NAME=Homarr
 APP_IMAGE=ghcr.io/ajnart/homarr
 APP_IMAGE_BRANCH=latest
+NETWORK=arr_default
--- homarr/CONTROL/post-install.sh
+++ homarr/CONTROL/post-install.sh
@@ -14,10 +14,13 @@ if [ ! -z $CONTAINER_TEST ]; then
         docker rm -f $CONTAINER_TEST
 fi
 
+echo "Create arr_default network"
+docker network inspect $NETWORK  >/dev/null || docker network create $NETWORK
+
 C_UID=$(id -u admin)
 ADMIN_GID=$(id -g admin)
 
-docker create -i -t --name=$APP_NAME \
+docker create -i -t --name=$APP_NAME --network=$NETWORK \
         -p 27575:7575 \
         -e PUID=$C_UID -e PGID=$ADMIN_GID -e DISABLE_ANALYTICS=true \
         -v /etc/localtime:/etc/localtime:ro \
