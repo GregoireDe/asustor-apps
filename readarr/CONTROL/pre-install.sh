5ca43bb12a8c18f8d0390b7006caef02e4230e58
GD <pea@invo.fr>
2026-02-07 14:05:39 +0100

    update version

--- readarr/CONTROL/pre-install.sh
+++ readarr/CONTROL/pre-install.sh
@@ -2,4 +2,18 @@
 
 echo "pre-install"
 
+. "/usr/local/AppCentral/prowlarr-docker/CONTROL/conf.sh"
+
+ARR_LIST="Jackett|Lidarr|Bazarr|Radarr|Overseer|Prowlarr|Sonarr|Readarr"
+ALL_ARR_LIST=$(docker container ps --format "table {{.Names}}" | grep -E $ARR_LIST | paste -sd' ')
+
+for i in $ALL_ARR_LIST; do
+    if [ ! "$(docker inspect $i -f '{{range $k, $v := .NetworkSettings.Networks}}{{$k}}{{end}}' |grep $NETWORK)" ]; then
+       echo "$i need to be attached."
+       docker network connect $NETWORK $i
+    else
+       echo "$i already attached."
+    fi
+done
+
 exit 0
