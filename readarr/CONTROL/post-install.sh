6ca70e1ee0184d4dcae7ccb70532021943afc427
GD <pea@invo.fr>
2026-02-09 15:18:11 +0100

    4.3 firmware as minimum

--- readarr/CONTROL/post-install.sh
+++ readarr/CONTROL/post-install.sh
@@ -4,18 +4,6 @@ echo "post-install"
 
 . "$APKG_PKG_DIR/CONTROL/conf.sh"
 
-ARR_LIST="Jackett|Lidarr|Bazarr|Radarr|Overseer|Prowlarr|Sonarr|Readarr"
-ALL_ARR_LIST=$(docker container ps --format "table {{.Names}}" | grep -E $ARR_LIST | paste -sd' ')
-
-for i in $ALL_ARR_LIST; do
-    if [ ! "$(docker inspect $i -f '{{range $k, $v := .NetworkSettings.Networks}}{{$k}}{{end}}' |grep $NETWORK)" ]; then
-       echo "$i need to be attached."
-       docker network connect $NETWORK $i
-    else
-       echo "$i already attached."
-    fi
-done
-
 docker pull $APP_IMAGE:$APP_IMAGE_BRANCH
 
 echo "Completed docker pull"
@@ -25,9 +13,6 @@ CONTAINER_TEST=$(docker container ls -a | grep $APP_NAME | awk '{print $1}')
 if [ ! -z $CONTAINER_TEST ]; then
         docker rm -f $CONTAINER_TEST
 fi
-
-echo "Create $NETWORK network"
-docker network inspect $NETWORK  >/dev/null || docker network create $NETWORK
 
 C_UID=$(id -u admin)
 ADMIN_GID=$(id -g admin)
@@ -44,5 +29,7 @@ docker create -i -t --name=$APP_NAME --network=$NETWORK \
         $APP_IMAGE:$APP_IMAGE_BRANCH
 
 docker start $APP_NAME
+
+. "$APKG_PKG_DIR/CONTROL/network-install.sh"
 
 exit 0
